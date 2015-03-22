34e3c4cf502bfc66bc3df5ba165f9f0bc65fa47b
Alex Koukoulas <alexk2410@hotmail.com>
2015-03-22 13:49:19 +0000

    Added new function in kinkycuts_extras

--- kinky_cuts_project/kinkyCuts/templatetags/kinkycuts_extras.py
+++ kinky_cuts_project/kinkyCuts/templatetags/kinkycuts_extras.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.inclusion_tag('kinkyCuts/creations.html')
 def get_creation_list():
     return{'creations': Creation.objects.order_by('-likes')[:3]}
+
+@register.filter
+def addstr(arg1, arg2):
+    """concatenate arg1 & arg2"""
+    return str(arg1) + str(arg2)
